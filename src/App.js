c32b671dd1ff34317d2be7607906cbe68218c881
TamBahri <59769742+TamBahri@users.noreply.github.com>
2020-06-06 12:43:21 +0100

    App.js and API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Countries from "./Components/Countries";
+import Search from "./Components/Search";
+import Filter from "./Components/Filter";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [hasError, setErrors] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchCountry, setSearchCountry] = useState("");
+
+  async function fetchData(url) {
+    setIsLoading(true);
+    await fetch(url)
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((err) => setErrors(err));
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    fetchData("https://restcountries.eu/rest/v2/all");
+  }, []);
+
+  const filteredData = data.filter(
+    (country) =>
+      country.name.toLowerCase().includes(searchCountry.toLowerCase()) ||
+      country.capital.toLowerCase().includes(searchCountry.toLowerCase())
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h3>Where in the world?</h3>
+        <a className="darkBtn" href="/">
+          <i className="fas fa-moon"></i>Dark Mode
         </a>
       </header>
+      <div className="main-container">
+        <div className="SearchAndFilter">
+          <Search
+            searchCountry={searchCountry}
+            setSearchCountry={setSearchCountry}
+          />
+          <Filter />
+        </div>
+
+        <Countries data={filteredData} err={hasError} loading={isLoading} />
+      </div>
     </div>
   );
 }
